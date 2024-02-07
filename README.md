e675924aca43d11766523fb66230ae5607ee267f
Brian Shef <brian.shef@rewardstyle.com>
2024-02-07 11:14:50 -0600

    Add simple Makefile

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # weathertop
 An experimental early-warning system to be used with the game Foxhole. Named after the famous, ancient watchtower in Lord of the Rings.
+
+## Running locally
+
+```
+make run
+```
+
+### Cleanup
+
+```
+make clean
+```
+
---
+++ main.py
@@ -0,0 +1,12 @@
+# (c) Brian Shef 2024
+
+import logging
+
+
+def main():
+	logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s]: %(message)s')
+	logging.info('Weathertop (c) Brian Shef 2024')
+
+if __name__ == '__main__':
+	main()
+
